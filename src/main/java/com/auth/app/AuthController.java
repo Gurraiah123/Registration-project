f5b7d0f9e712299bb23901b9b4cac9295111ae5d
Gurraiah123 <142591366+Gurraiah123@users.noreply.github.com>
2026-05-05 13:06:19 +0530

    Update AuthController.java

--- src/main/java/com/auth/app/AuthController.java
+++ src/main/java/com/auth/app/AuthController.java
@@ -1,15 +1,13 @@
 package com.auth.app;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
-public class AuthController {
+public class HomeController {
 
-    @GetMapping("/health")
-    public String health() {
-        return "Application Running";
+    @GetMapping("/")
+    public String home() {
+        return "App is working";
     }
 }
